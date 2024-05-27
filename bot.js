d6a677973724bc8fe6f0998ff7ed7756a3638f9d
Alex <atdanakis@gmail.com>
2024-05-27 09:52:25 -0500

    Removed extraneous helperFile reference

--- bot.js
+++ bot.js
@@ -14,7 +14,6 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ],
 });
-const helperFile = require('./helperFile');
 
 //sets up usage of commands from command folder
 client.commands = new Collection();
