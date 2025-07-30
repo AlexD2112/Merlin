515ca4d6e0e8d5e625bd3235acd82a2fd41cf9be
AlexD2112 <107415752+AlexD2112@users.noreply.github.com>
2025-07-30 16:11:22 -0500

    Not sure what this is, might fuck things up

--- helperFile.js
+++ helperFile.js
@@ -99,7 +99,75 @@ async function addTo10RecipeIngredients() {
     await dbm.saveCollection('recipes', recipes);
 }
 
-addTo10RecipeIngredients();
+async function resetIncomeCD() {
+    let collectionName = 'characters';
+    let data = await dbm.loadCollection(collectionName);
+    //Set date to midnight, february 13
+    let now = new Date();
+    now = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - 1, 0, 0, 0, 0));
+    let nextResetTimes = new Map();
+
+    // Define the start date
+    let startDate = new Date(0); // Unix epoch start date: January 1, 1970
+
+    for (let [_, charData] of Object.entries(data)) {
+      // Always reset the primary incomeAvailable field
+      //charData.incomeAvailable = true;
+      
+      // Reset additional incomeAvailable fields based on their intervals
+      for (let key in charData) {
+        if (key.startsWith('incomeAvailable') && key !== 'incomeAvailable') {
+          let delay = key.replace('incomeAvailable', '');
+
+          if (!nextResetTimes.has(delay)) {
+            let delayAmount = parseInt(delay.slice(0, -1));
+            let delayUnit = delay.slice(-1);
+            let nextCycleTime = new Date(startDate);
+
+            while (nextCycleTime < now) {
+                console.log(nextCycleTime, now)
+                switch (delayUnit) {
+                    case 'D':
+                        nextCycleTime.setUTCDate(nextCycleTime.getUTCDate() + delayAmount);
+                        break;
+                    case 'W':
+                        nextCycleTime.setUTCDate(nextCycleTime.getUTCDate() + 7 * delayAmount);
+                        break;
+                    case 'M':
+                        nextCycleTime.setUTCMonth(nextCycleTime.getUTCMonth() + delayAmount);
+                        break;
+                    case 'Y':
+                        nextCycleTime.setUTCFullYear(nextCycleTime.getUTCFullYear() + delayAmount);
+                        break;
+                }
+            }
+
+            console.log(nextCycleTime);
+            console.log(nextResetTimes);
+            console.log(delay);
+            console.log(nextResetTimes.get(delay));
+            console.log("Next Cycle Day" + nextCycleTime.getUTCDate() + " Next Cycle Month" + nextCycleTime.getUTCMonth() + " Next Cycle Year" + nextCycleTime.getUTCFullYear());
+            console.log("Now Day" + now.getUTCDate() + " Now Month" + now.getUTCMonth() + " Now Year" + now.getUTCFullYear());
+
+            nextResetTimes.set(delay, nextCycleTime.getUTCDate() === now.getUTCDate() &&
+                                      nextCycleTime.getUTCMonth() === now.getUTCMonth() &&
+                                      nextCycleTime.getUTCFullYear() === now.getUTCFullYear());
+
+            console.log(nextResetTimes.get(delay));
+          }
+          if (nextResetTimes.get(delay)) {
+            charData[key] = true;
+          }
+        }
+      }
+    }
+    
+    await dbm.saveCollection(collectionName, data);
+  }
+
+resetIncomeCD();
+
+// addTo10RecipeIngredients();
 
 module.exports = {
     addNeedNoneOfRolesToShop,
